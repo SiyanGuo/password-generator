e6c9f9456668b7300982b2ead002b2e23ff5f7ef
Siyan Guo <siyang@Hey-There.local>
2021-07-03 00:35:41 -0400

    update password generator function

--- script.js
+++ script.js
@@ -1,28 +1,80 @@
 // Assignment code here
-pseudo code
+// pseudo code
 
-var lowerCase = ['a'];
-var upperCase = ['A'];
-var number= ['1','2','3','4','5','6','7','8','9','0'];
-//31 length
+
+// length 26
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//length 10
+var number = '1234567890';
+//length 31
 var specialCha = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-var pwd = [];
+var chaType = ''
+var pwd = '';
+
+
+// function confirmChaType() {
+//
+// }
+//console.log(lowerCase+number);
 
 function generatePassword() {
 
-var length = parseInt(window.prompt('How long is the password? (mininal 8 characters and maximum 128 characters)'));
+  var passwordLength = parseInt(window.prompt('How long is the password? (minimum 8 and maximum 128 characters)'));
 
-if (length! || length>128 || length<8){
-  alert('Please input a valid number between 8 - 128');
-  generatePassword();
-} else {
-  var isLower = window.confirm('Does it contain lower case?');
-  var isUpper = window.confirm('Does it contain upper case?');
-  var isNumber = window.confirm('Does it contain numbers?');
-  var isSpecialCha = window.confirm('Does it contain special characters?');
+  if (!passwordLength || passwordLength > 128 || passwordLength < 8) {
+    alert('Please input a valid number between 8 - 128.');
+    generatePassword();
+  }
+
+    var isLower = window.confirm('Does it contain lower case?');
+    var isUpper = window.confirm('Does it contain upper case?');
+    var isNumber = window.confirm('Does it contain numbers?');
+    var isSpecialCha = window.confirm('Does it contain special characters?');
+
+
+  if (!isLower && !isUpper && !isNumber && !isSpecialCha) {
+    alert('Please select at least one character type.');
+    // confirmChaType();
+  }
+
+  if (isLower) {
+    var chosenLowerCase = lowerCase.charAt(Math.floor(Math.random() * 26) + 1);
+    pwd += chosenLowerCase;
+    chaType += lowerCase;
+  }
+
+  if (isUpper) {
+    var chosenUpperCase = upperCase.charAt(Math.floor(Math.random() * 26) + 1);
+    pwd += chosenUpperCase;
+    chaType += upperCase;
+  }
+
+  if (isNumber) {
+    var chosenNumber = number.charAt(Math.floor(Math.random() * 10) + 1);
+    pwd += chosenNumber;
+    chaType +=number;
+  }
+
+  if (isSpecialCha) {
+    var chosenSpecialCha = specialCha.charAt(Math.floor(Math.random() * 31) + 1);
+    pwd += chosenSpecialCha;
+    chaType += specialCha;
+  }
+
+var remainingLength = passwordLength - pwd.length
+
+console.log(remainingLength);
+
+for(i=0; i<remainingLength; i++){
+
+var remainingCha = chaType.charAt(Math.floor(Math.random() * chaType.length) + 1);
+
+  pwd+=remainingCha;
+
 }
-
+return pwd;
 }
 
 
